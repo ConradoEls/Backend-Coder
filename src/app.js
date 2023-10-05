dffd255cf248e43bbd4a6d9a2d27fb8b95bcd273
Conrado Els <conradoels@gmail.com>
2023-10-05 09:40:33 -0300

    modifico archivo app y ubicacion de carpeta routes

--- src/app.js
+++ src/app.js
@@ -1,17 +1,13 @@
 import express from "express";
-import productsRouter from "../routes/products.js";
-import cartsRouter from "../routes/carts.js";
+import productsRouter from "./routes/products.js";
+import cartsRouter from "./routes/carts.js";
 
 const PORT = 8080;
 const app = express();
 
 app.use(express.json());
-productsRouter.use(express.json());
-cartsRouter.use(express.json());
-
 app.use(express.urlencoded({ extended: true }));
-productsRouter.use(express.urlencoded({ extended: true }));
-cartsRouter.use(express.urlencoded({ extended: true }));
+
 
 app.use("/api/products", productsRouter);
 app.use("/api/carts", cartsRouter);
